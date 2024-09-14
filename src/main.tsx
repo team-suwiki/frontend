d7c78f5aac869e732d318abee7cd55b88d6342ab
Chung-il <wer4272@nate.com>
2024-09-14 15:24:18 +0900

    feat: tanstack query v5 (#98)
    공지사항 부분부터 점진적 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import './styles/globalStyle.css';
 
+import {
+  QueryClient as QueryClient5,
+  QueryClientProvider as QueryClientProvider5,
+} from '@tanstack/react-query';
 import axios from 'axios';
 import AsyncBoundary from 'components/AsyncBoundary';
-import { CACHE_TIME } from 'constants/cacheTime';
 import { BadGateway } from 'pages';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { initialize } from 'react-ga';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import {
+  QueryClient as QueryClient4,
+  QueryClientProvider as QueryClientProvider4,
+} from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
 
@@ -15,15 +21,8 @@ import App from './App';
 
 initialize('G-KG7KQ8K3GP');
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      cacheTime: CACHE_TIME.MINUTE_5,
-      staleTime: CACHE_TIME.MINUTE_5,
-    },
-  },
-});
+export const queryClient = new QueryClient4();
+const queryClient5 = new QueryClient5();
 const PROXY_URL = window.location.hostname === 'localhost' ? '/api' : '/proxy';
 
 axios.defaults.baseURL = PROXY_URL;
@@ -32,12 +31,14 @@ axios.defaults.withCredentials = true;
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AsyncBoundary pendingFallback={<></>} rejectedFallback={() => <BadGateway />}>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <App />
-          <ReactQueryDevtools />
-        </RecoilRoot>
-      </QueryClientProvider>
+      <QueryClientProvider4 client={queryClient}>
+        <QueryClientProvider5 client={queryClient5}>
+          <RecoilRoot>
+            <App />
+            <ReactQueryDevtools />
+          </RecoilRoot>
+        </QueryClientProvider5>
+      </QueryClientProvider4>
     </AsyncBoundary>
   </React.StrictMode>,
 );
