fea834f6ba211ebb812ae5deda190dc9ab8edc3a
Chung-il Jung <jungchungil7@gmail.com>
2025-01-30 16:52:58 +0800

    fix: nav 스크롤 버그픽스 및 스타일 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,67 +11,69 @@ const Nav = () => {
 
   return (
     <Navbar>
-      <picture>
-        <source srcSet="/images/logo.avif" type="image/avif" />
-        <source srcSet="/images/logo.webp" type="image/webp" />
-        <source srcSet="/images/logo.png" type="image/png" />
-        <NavLogo
-          src="/images/logo.png"
-          alt="logo"
-          width={110}
-          height={30}
-          onClick={() => navigate('/')}
-        />
-      </picture>
-      <MobileIcon onClick={handleClick}>
-        {click ? (
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 16 16"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
-            ></path>
-          </svg>
-        ) : (
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 512 512"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
-          </svg>
-        )}
-      </MobileIcon>
+      <NavLogoContainer>
+        <picture>
+          <source srcSet="/images/logo.avif" type="image/avif" />
+          <source srcSet="/images/logo.webp" type="image/webp" />
+          <source srcSet="/images/logo.png" type="image/png" />
+          <NavLogo
+            src="/images/logo.png"
+            alt="logo"
+            width={110}
+            height={30}
+            onClick={() => navigate('/')}
+          />
+        </picture>
+        <MobileIcon onClick={handleClick}>
+          {click ? (
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 16 16"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 512 512"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
+            </svg>
+          )}
+        </MobileIcon>
 
-      <NavMenu onClick={handleClick} click={click}>
-        <NavLinks onClick={() => navigate('/notice')}>공지사항</NavLinks>
-        {!isLogin ? (
-          <NavLinks onClick={() => navigate('/login')}>로그인</NavLinks>
-        ) : (
-          <NavLinks onClick={logout}>로그아웃</NavLinks>
-        )}
-        {!isLogin ? (
-          <NavLinks id="signup" onClick={() => navigate('/signup')}>
-            회원가입
-          </NavLinks>
-        ) : (
-          <NavLinks id="signup" onClick={() => navigate('/myinfo')}>
-            내 정보
-          </NavLinks>
-        )}
-      </NavMenu>
+        <NavMenu onClick={handleClick} click={click}>
+          <NavLinks onClick={() => navigate('/notice')}>공지사항</NavLinks>
+          {!isLogin ? (
+            <NavLinks onClick={() => navigate('/login')}>로그인</NavLinks>
+          ) : (
+            <NavLinks onClick={logout}>로그아웃</NavLinks>
+          )}
+          {!isLogin ? (
+            <NavLinks id="signup" onClick={() => navigate('/signup')}>
+              회원가입
+            </NavLinks>
+          ) : (
+            <NavLinks id="signup" onClick={() => navigate('/myinfo')}>
+              내 정보
+            </NavLinks>
+          )}
+        </NavMenu>
+      </NavLogoContainer>
     </Navbar>
   );
 };
@@ -79,37 +81,26 @@ const Nav = () => {
 export default Nav;
 
 const Navbar = styled.nav`
-  background: #ffffff;
-  margin: 0 auto;
-  height: 110px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2rem;
+  min-height: 60px;
+  position: sticky;
   top: 0;
-  z-index: 999;
+  z-index: 10;
+  padding: 20px;
+  backdrop-filter: blur(8px);
+  background: rgba(255, 255, 255, 0.8);
+`;
 
-  justify-content: space-around;
-  border-bottom: 1.5px solid #e0e0e0;
-
-  @media screen and (max-width: 550px) {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
-    justify-content: space-between;
-  }
+const NavLogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1024px;
+  margin: 0 auto;
 `;
 
 const NavLogo = styled.img`
   &:hover {
     cursor: pointer;
-  }
-
-  @media screen and (max-width: 550px) {
-    font-size: var(--font-medium);
-    font-weight: var(--weight-semi-bold);
   }
 `;
 
@@ -119,21 +110,21 @@ const MobileIcon = styled.div`
   color: #336af8;
   display: none;
 
-  @media screen and (max-width: 550px) {
+  @media screen and (max-width: 768px) {
     display: block;
   }
 `;
 
-const NavMenu = styled.nav<{ click: boolean }>`
+const NavMenu = styled.div<{ click: boolean }>`
   display: flex;
   gap: 2rem;
 
-  @media screen and (max-width: 550px) {
+  @media screen and (max-width: 768px) {
     flex-direction: column;
     width: 100%;
-    height: 90vh;
+    height: 100vh;
     position: absolute;
-    top: 120px;
+    top: 60px;
     left: ${({ click }) => (click ? 0 : '-100%')};
     transition: all 0.5s ease;
     background: #ffffff;
@@ -143,6 +134,7 @@ const NavMenu = styled.nav<{ click: boolean }>`
 
 const NavLinks = styled.div`
   font-size: 1rem;
+  font-weight: 500;
   &:hover {
     cursor: pointer;
   }
@@ -150,7 +142,7 @@ const NavLinks = styled.div`
     color: #336af8;
   }
 
-  @media screen and (max-width: 550px) {
+  @media screen and (max-width: 768px) {
     text-align: center;
     padding: 2rem;
     display: table;
